c97f090b7ed6648f72f5ec3cd175f5f09fcd28ff
Yan Kuang <YTKme@Outlook.com>
2023-07-12 21:31:09 -0700

    Update Core logging function

--- Core/logging/Logging/test/test_logger.py
+++ Core/logging/Logging/test/test_logger.py
@@ -3,13 +3,11 @@
 This module test functionality for the logger.
 """
 
-import logging
-
 from Core.logging.Logging import logger
 
 
-def test_initialize_success():
-    """Test a successful initialize of the logger class
+def test_initialize():
+    """Test initialize of the logger class
 
     Create an instance of Logger and check to make sure the object
     is of Logger type.
@@ -19,19 +17,8 @@ def test_initialize_success():
     assert isinstance(test_logger, logger.Logger)
 
 
-def test_initialize_failure():
-    """Test a failure initialize of the logger class
-
-    Create an instance of logging.Logger and check to see if the
-    object is of Logger type.
-    """
-
-    test_logger = logging.Logger('test-logger')
-    assert not isinstance(test_logger, logger.Logger)
-
-
 def test_color_success():
-    """Test a successful display of log messages and color
+    """Test display of log messages and color
 
     Create an instance of Logger and log messages using different
     level to display different color.

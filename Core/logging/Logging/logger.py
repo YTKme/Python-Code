99a6f57d112196eee7a074e51e00ae7836cc7eba
Yan Kuang <YTKme@Outlook.com>
2023-07-12 21:31:09 -0700

    Update Core logging function

--- Core/logging/Logging/logger.py
+++ Core/logging/Logging/logger.py
@@ -1,6 +1,6 @@
 """Logger Module
 
-This module implements a custom Logger.
+This module implements the Logger.
 """
 
 import logging
@@ -18,51 +18,51 @@ INFO = logging.INFO
 DEBUG = logging.DEBUG
 NOTSET = logging.NOTSET
 
-RECORD_FORMAT = '[%(name)s %(levelname)s %(asctime)s] %(message)s'
-DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
+_RECORD_FORMAT = '[%(name)s %(levelname)s %(asctime)s] %(message)s'
+_DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
 
-
-class ColorCode:
-    """Color Code for logging"""
-
+_COLOR_CODE = {
     # Reset
-    RESET = '\x1b[0m'
+    'RESET': '\x1b[0m',
     # Foreground
-    FOREGROUND_BLACK = '\x1b[30m'
-    FOREGROUND_RED = '\x1b[31m'
-    FOREGROUND_GREEN = '\x1b[32m'
-    FOREGROUND_YELLOW = '\x1b[33m'
-    FOREGROUND_BLUE = '\x1b[34m'
-    FOREGROUND_MAGENTA = '\x1b[35m'
-    FOREGROUND_CYAN = '\x1b[36m'
-    FOREGROUND_WHITE = '\x1b[37m'
-    FOREGROUND_DEFAULT = '\x1b[39m'
+    'FOREGROUND_BLACK': '\x1b[30m',
+    'FOREGROUND_RED': '\x1b[31m',
+    'FOREGROUND_GREEN': '\x1b[32m',
+    'FOREGROUND_YELLOW': '\x1b[33m',
+    'FOREGROUND_BLUE': '\x1b[34m',
+    'FOREGROUND_MAGENTA': '\x1b[35m',
+    'FOREGROUND_CYAN': '\x1b[36m',
+    'FOREGROUND_WHITE': '\x1b[37m',
+    'FOREGROUND_DEFAULT': '\x1b[39m',
     # Background
-    BACKGROUND_BLACK = '\x1b[40m'
-    BACKGROUND_RED = '\x1b[41m'
-    BACKGROUND_GREEN = '\x1b[42m'
-    BACKGROUND_YELLOW = '\x1b[43m'
-    BACKGROUND_BLUE = '\x1b[44m'
-    BACKGROUND_MAGENTA = '\x1b[45m'
-    BACKGROUND_CYAN = '\x1b[46m'
-    BACKGROUND_WHITE = '\x1b[47m'
-    BACKGROUND_DEFAULT = '\x1b[49m'
+    'BACKGROUND_BLACK': '\x1b[40m',
+    'BACKGROUND_RED': '\x1b[41m',
+    'BACKGROUND_GREEN': '\x1b[42m',
+    'BACKGROUND_YELLOW': '\x1b[43m',
+    'BACKGROUND_BLUE': '\x1b[44m',
+    'BACKGROUND_MAGENTA': '\x1b[45m',
+    'BACKGROUND_CYAN': '\x1b[46m',
+    'BACKGROUND_WHITE': '\x1b[47m',
+    'BACKGROUND_DEFAULT': '\x1b[49m',
     # Style
-    STYLE_BOLD = '\x1b[1m'
-    STYLE_DIM = '\x1b[2m'
-    STYLE_UNDERLINED = '\x1b[4m'
-    STYLE_BLINK = '\x1b[5m'
-    STYLE_REVERSE = '\x1b[7m'
-    STYLE_HIDDEN = '\x1b[8m'
-    STYLE_DEFAULT = '\x1b[22m'
-    
-    NOTSET = RESET
-    DEBUG = f'{FOREGROUND_CYAN}'
-    INFO = f'{FOREGROUND_GREEN}'
-    WARNING = f'{FOREGROUND_YELLOW}'
-    SUCCESS = f'{FOREGROUND_GREEN}'
-    ERROR = f'{FOREGROUND_RED}'
-    CRITICAL = f'{FOREGROUND_RED}{BACKGROUND_WHITE}'
+    'STYLE_BOLD': '\x1b[1m',
+    'STYLE_DIM': '\x1b[2m',
+    'STYLE_UNDERLINED': '\x1b[4m',
+    'STYLE_BLINK': '\x1b[5m',
+    'STYLE_REVERSE': '\x1b[7m',
+    'STYLE_HIDDEN': '\x1b[8m',
+    'STYLE_DEFAULT': '\x1b[22m',
+    }
+
+_LEVEL_COLOR_CODE = {
+    'NOTSET': _COLOR_CODE['RESET'],
+    'DEBUG': _COLOR_CODE['FOREGROUND_CYAN'],
+    'INFO': _COLOR_CODE['FOREGROUND_GREEN'],
+    'WARNING': _COLOR_CODE['FOREGROUND_YELLOW'],
+    'SUCCESS': _COLOR_CODE['FOREGROUND_GREEN'],
+    'ERROR': _COLOR_CODE['FOREGROUND_RED'],
+    'CRITICAL': f"{_COLOR_CODE['FOREGROUND_RED']}{_COLOR_CODE['BACKGROUND_WHITE']}",
+    }
 
 
 class LoggerFormatter(logging.Formatter):
@@ -73,28 +73,50 @@ class LoggerFormatter(logging.Formatter):
 
     def __init__(
         self,
-        record_format: str = RECORD_FORMAT,
-        date_format: str = DATE_FORMAT
+        record_format: str = _RECORD_FORMAT,
+        date_format: str = _DATE_FORMAT
         ) -> None:
         """Constructor
 
-        :param log_format: (str) the log format for the Formatter
+        :param record_format: (str) the record format for the Formatter
+        :type record_format: str, default to `RECORD_FORMAT` constant
         :param date_format: (str) the date format for the Formatter
+        :type date_format: str, default to `DATE_FORMAT` constant
         """
         # Call super class
         super().__init__(fmt=record_format, datefmt=date_format)
 
-        self.LEVEL_FORMAT = {
-            logging.DEBUG: f'{ColorCode.DEBUG}{record_format}{ColorCode.RESET}',
-            logging.INFO: f'{ColorCode.INFO}{record_format}{ColorCode.RESET}',
-            logging.WARNING: f'{ColorCode.WARNING}{record_format}{ColorCode.RESET}',
-            logging.ERROR: f'{ColorCode.ERROR}{record_format}{ColorCode.RESET}',
-            logging.CRITICAL: f'{ColorCode.CRITICAL}{record_format}{ColorCode.RESET}'
+        self._level_format = {
+            logging.DEBUG: (
+                f"{_LEVEL_COLOR_CODE['DEBUG']}"
+                f"{record_format}"
+                f"{_LEVEL_COLOR_CODE['NOTSET']}"
+                ),
+            logging.INFO: (
+                f"{_LEVEL_COLOR_CODE['INFO']}"
+                f"{record_format}"
+                f"{_LEVEL_COLOR_CODE['NOTSET']}"
+                ),
+            logging.WARNING: (
+                f"{_LEVEL_COLOR_CODE['WARNING']}"
+                f"{record_format}"
+                f"{_LEVEL_COLOR_CODE['NOTSET']}"
+                ),
+            logging.ERROR: (
+                f"{_LEVEL_COLOR_CODE['ERROR']}"
+                f"{record_format}"
+                f"{_LEVEL_COLOR_CODE['NOTSET']}"
+                ),
+            logging.CRITICAL: (
+                f"{_LEVEL_COLOR_CODE['CRITICAL']}"
+                f"{record_format}"
+                f"{_LEVEL_COLOR_CODE['NOTSET']}"
+                ),
             }
 
         self.date_format = date_format
 
-    
+
     def format(self, record: LogRecord) -> str:
         """Format the specified record as text (redefined)
 
@@ -103,7 +125,7 @@ class LoggerFormatter(logging.Formatter):
 
         :return: (str) the formatted record
         """
-        log_format = self.LEVEL_FORMAT.get(record.levelno)
+        log_format = self._level_format.get(record.levelno)
         formatter = logging.Formatter(fmt=log_format, datefmt=self.date_format)
 
         return formatter.format(record)
@@ -124,12 +146,14 @@ class Logger(logging.Logger):
         # level: int | str = NOTSET) -> None:
         """Constructor
         
-        :param name: (str) the name of the logger
-        :param level: (int) or (str) initialize the level of the logger
+        :param name: the name of the logger
+        :type name: str
+        :param level: initialize the level of the logger
+        :type level: int or str, default to `NOTSET`
         """
 
         # Call super class
-        super(Logger, self).__init__(name=name, level=level)
+        super().__init__(name=name, level=level)
 
         # Initialize handler
         self._initialize_handler()
@@ -163,16 +187,18 @@ class Logger(logging.Logger):
 
     def set_formatter(
         self,
-        record_format: str = RECORD_FORMAT,
-        date_format: str = DATE_FORMAT
+        record_format: str = _RECORD_FORMAT,
+        date_format: str = _DATE_FORMAT
         ) -> None:
         """Set Formatter for Logger
 
         Enable user to set a different format for the log record and the
         log date.
 
-        :param record_format: (str) the new format for the log record
-        :param date_format: (str) the new format for the log date
+        :param record_format: the new format for the log record
+        :type record_format: str, default to `RECORD_FORMAT` constant
+        :param date_format: the new format for the log date
+        :type date_format: str, default to `DATE_FORMAT` constant
         """
 
         self.handlers.clear()
@@ -194,3 +220,87 @@ class Logger(logging.Logger):
                 )
             )
         self.addHandler(self.stderr_handler)
+
+
+root = Logger('logger')
+
+
+def critical(
+    message: str,
+    *args,
+    **kwargs
+    ):
+    """Log `message` with severity `CRITICAL` level.
+
+    :param message: the message to log
+    :type message: str
+    """
+    root.critical(message, *args, **kwargs)
+
+
+def error(
+    message: str,
+    *args,
+    **kwargs
+    ):
+    """Log `message` with severity `ERROR` level.
+
+    :param message: the message to log
+    :type message: str
+    """
+    root.error(message, *args, **kwargs)
+
+
+def warning(
+    message: str,
+    *args,
+    **kwargs
+    ):
+    """Log `message` with severity `WARNING` level.
+
+    :param message: the message to log
+    :type message: str
+    """
+    root.warning(message, *args, **kwargs)
+
+
+def info(
+    message: str,
+    *args,
+    **kwargs
+    ):
+    """Log `message` with severity `INFO` level.
+
+    :param message: the message to log
+    :type message: str
+    """
+    root.info(message, *args, **kwargs)
+
+
+def debug(
+    message: str,
+    *args,
+    **kwargs
+    ):
+    """Log `message` with severity `DEBUG` level.
+
+    :param message: the message to log
+    :type message: str
+    """
+    root.debug(message, *args, **kwargs)
+
+
+def log(
+    level,
+    message: str,
+    *args,
+    **kwargs
+    ):
+    """Log `message` with give `level` severity.
+
+    :param level: the severity level for the log
+    :type level: int, use predefined log level
+    :param message: the message to log
+    :type message: str
+    """
+    root.log(level, message, *args, **kwargs)

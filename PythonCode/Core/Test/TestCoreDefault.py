5bc714b022e6bf0555830c801b8187389c88bc52
Yan Kuang <YTKme@Outlook.com>
2024-03-20 11:30:27 -0700

    Move pytest Order code to PythonCode Core Test

---
+++ PythonCode/Core/Test/TestCoreDefault.py
@@ -0,0 +1,11 @@
+"""
+Test Core Default
+~~~~~~~~~~~~~~~~~
+"""
+
+class TestCoreDefault:
+    """Test Core Default"""
+
+    def test_core_default(self):
+        """Test Core Default"""
+        assert True

5de74b88afe608420cbf2e789d669ee5751644cf
Yan Kuang <YTKme@Outlook.com>
2024-03-22 15:28:31 -0700

    Add AsynchronousQueueDelta module and test code

---
+++ PythonCode/Asynchronous/AsynchronousQueueDelta.py
@@ -0,0 +1,94 @@
+"""
+Asynchronous Queue Delta
+~~~~~~~~~~~~~~~~~~~~~~~~
+
+The AsynchronousQueueDelta module provide example(s) for the
+`asyncio.Queue` module with class. It uses `async with TaskGroup()` for
+task creation, along with `asyncio.BoundedSemaphore` as limiter for
+concurrent task(s).
+"""
+
+import asyncio
+from asyncio import BoundedSemaphore, Queue, Semaphore, TaskGroup
+
+import tealogger
+
+
+tealogger.set_level(tealogger.DEBUG)
+
+
+class AsynchronousQueueDelta:
+    """Asynchronous Queue Delta Class"""
+
+    def __init__(self, *args, **kwargs):
+        """Constructor"""
+        ...
+
+    def __new__(cls, *args, **kwargs):
+        """New"""
+        return super().__new__(cls)
+
+    async def fetch_recursive(
+        self,
+        source_list: list[str],
+        # destination_list: list[str],
+        maximum_task: int = 10,
+    ):
+        """Fetch Recursive
+
+        :param source: The source (Remote) path
+        :type source_list: list[str]
+        :param destination: The destination (Local) path
+        :type destination: list[str]
+        :param maximum_task: The `maximum_task` to fetch concurrently,
+            defaults to 10
+        :type maximum_task: int
+        """
+        tealogger.info('Fetch Recursive')
+
+        limiter = BoundedSemaphore(maximum_task)
+
+        # Create a `result_queue` to store the result(s) of the fetch
+        result_queue = Queue()
+
+        async with TaskGroup() as group:
+            # Create `maximum_task` of `fetch_query` worker task(s)
+            # Store them in a `task_list`
+            task_list = [
+                group.create_task(
+                    self.fetch_query(
+                        name=f'Worker-{index + 1}',
+                        source=source,
+                        result_queue=result_queue,
+                        limiter=limiter,
+                    )
+                ) for index, source in enumerate(source_list)
+            ]
+
+
+    async def fetch_query(
+        self,
+        name: str,
+        source: str,
+        result_queue: Queue,
+        limiter: Semaphore,
+    ):
+        """Fetch Query
+
+        :param name: The `name` of the worker
+        :type name: str
+        :param source: The `source` to fetch
+        :type source: str
+        :param result_queue: The `result_queue` to store the result
+        :type result_queue: Queue
+        """
+        async with limiter:
+            tealogger.debug(f'{name} Source: {source}')
+
+            # Simulate the work
+            await asyncio.sleep(3)
+
+            # Store the result
+            result = f'{name} Result: {source}'
+            await result_queue.put(result)
+            tealogger.debug(f'Finished Task: {name}')
---
+++ PythonCode/Asynchronous/Test/TestAsynchronousQueueDelta.py
@@ -0,0 +1,42 @@
+"""
+Test Asynchronous Queue Delta
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+
+import pytest
+
+from PythonCode.Asynchronous.AsynchronousQueueDelta import AsynchronousQueueDelta
+
+
+class TestAsynchronousQueueDelta:
+    """Test Asynchronous Queue Delta"""
+
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize(
+        'source_list', [
+            [
+                'https://httpbin.org/anything/Delta/1',
+                'https://httpbin.org/anything/Delta/2',
+                'https://httpbin.org/anything/Delta/3',
+                'https://httpbin.org/anything/Delta/4',
+                'https://httpbin.org/anything/Delta/5',
+                'https://httpbin.org/anything/Delta/6',
+                'https://httpbin.org/anything/Delta/7',
+                'https://httpbin.org/anything/Delta/8',
+                'https://httpbin.org/anything/Delta/9',
+                'https://httpbin.org/anything/Delta/10',
+                'https://httpbin.org/anything/Delta/11',
+                'https://httpbin.org/anything/Delta/12',
+            ],
+        ]
+    )
+    async def test_fetch_recursive(
+        self,
+        source_list: list[str],
+    ):
+        """Test Fetch Recursive"""
+        delta = AsynchronousQueueDelta()
+
+        await delta.fetch_recursive(
+            source_list=source_list,
+        )

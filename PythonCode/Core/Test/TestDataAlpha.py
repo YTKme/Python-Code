67ebf323aedc62c9ee78f2e80a806d8402b57cba
Yan Kuang <YTKme@Outlook.com>
2024-04-18 10:23:17 -0700

    Update TestDataAlpha module for Core package

--- PythonCode/Core/Test/TestDataAlpha.py
+++ PythonCode/Core/Test/TestDataAlpha.py
@@ -5,4 +5,8 @@ Test Data Alpha
 
 class TestDataAlpha:
     """Test Data Alpha"""
-    pass
+
+    def test_data_alpha_base(self):
+        """Test Data Alpha Base"""
+        print('Test Data Alpha Base')
+        assert True

19c5c3e9b98c92199c466ab4f0a3e2ba7e474c18
Yan Kuang <YTKme@Outlook.com>
2024-05-02 22:25:06 -0700

    Add Concurrency example for Thread and AsyncIO

---
+++ PythonCode/Concurrency/Test/TestThreadAsyncIO.py
@@ -0,0 +1,14 @@
+"""
+Test Thread Async IO
+~~~~~~~~~~~~~~~~~~~~
+
+The TestThreadAsyncIO module execute test(s) for the `ThreadAsyncIO`
+module.
+"""
+
+import pytest
+
+
+def test_fetch_worker():
+    """Test Fetch Worker"""
+    pass
---
+++ PythonCode/Concurrency/ThreadAsyncIO.py
@@ -0,0 +1,55 @@
+"""
+Thread Async IO
+~~~~~~~~~~~~~~~
+
+The ThreadAsyncIO module provide example(s) and comparison(s) for the
+`threading` and the `asyncio` module.
+"""
+
+import requests
+import timeit
+
+
+WEBSITE_LIST = [
+    'https://api.apis.guru/v2/specs/1forge.com/0.0.1/swagger.json',
+    'https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0',
+    'https://api.apis.guru/v2/specs/1password.com/events/1.0.0/openapi.json',
+    'https://api.apis.guru/v2/specs/1password.local/connect/1.5.7/openapi.json',
+    'https://api.apis.guru/v2/specs/6-dot-authentiqio.appspot.com/6/openapi.json',
+    'https://api.apis.guru/v2/specs/ably.io/platform/1.1.0/openapi.json',
+    'https://api.apis.guru/v2/specs/ably.net/control/1.0.14/openapi.json',
+    'https://api.apis.guru/v2/specs/abstractapi.com/geolocation/1.0.0/openapi.json',
+    'https://api.apis.guru/v2/specs/adafruit.com/2.0.0/swagger.json',
+    'https://api.apis.guru/v2/specs/adobe.com/aem/3.7.1-pre.0/openapi.json',
+    'https://api.apis.guru/v2/specs/adyen.com/AccountService/6/openapi.json',
+    'https://api.apis.guru/v2/specs/adyen.com/BalanceControlService/1/openapi.json',
+    'https://api.apis.guru/v2/specs/adyen.com/BalancePlatformConfigurationNotification-v1/1/openapi.json',
+    'https://api.apis.guru/v2/specs/adyen.com/BalancePlatformPaymentNotification-v1/1/openapi.json',
+    'https://api.apis.guru/v2/specs/adyen.com/BalancePlatformReportNotification-v1/1/openapi.json',
+    'https://api.apis.guru/v2/specs/adyen.com/BalancePlatformService/2/openapi.json',
+    'https://api.apis.guru/v2/specs/adyen.com/BalancePlatformTransferNotification-v3/3/openapi.json',
+    'https://api.apis.guru/v2/specs/adyen.com/BinLookupService/54/openapi.json',
+    'https://api.apis.guru/v2/specs/adyen.com/CheckoutService/70/openapi.json',
+    'https://api.apis.guru/v2/specs/adyen.com/CheckoutUtilityService/1/openapi.json',
+    'https://api.apis.guru/v2/specs/adyen.com/DataProtectionService/1/openapi.json',
+]
+
+
+def fetch_website_thread():
+    """Fetch Website Thread"""
+
+
+
+
+def fetch_worker(website: str):
+    """Fetch Worker
+
+    :param website: The URL (Uniform Resource Locator) for the website
+    :type website: str
+    """
+
+    print(f'Fetch Worker Website: {website}')
+    start = timeit.default_timer()
+    response = requests.get(website)
+    stop = timeit.default_timer()
+    print(f'Fetch Worker Time: {stop - start:.2f}')

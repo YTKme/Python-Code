e43d5beb7fd25f299f0f9264a50941255f283b0d
Yan Kuang <YTKme@Outlook.com>
2024-05-03 13:34:09 -0700

    Add threading function for ThreadAsyncIO module

--- PythonCode/Concurrency/Test/TestThreadAsyncIO.py
+++ PythonCode/Concurrency/Test/TestThreadAsyncIO.py
@@ -5,10 +5,18 @@ Test Thread Async IO
 The TestThreadAsyncIO module execute test(s) for the `ThreadAsyncIO`
 module.
 """
+import timeit
 
 import pytest
 
+from PythonCode.Concurrency import ThreadAsyncIO
 
-def test_fetch_worker():
-    """Test Fetch Worker"""
-    pass
+
+def test_fetch_website_thread():
+    """Test Fetch Website Thread"""
+
+    # Execute
+    start = timeit.default_timer()
+    ThreadAsyncIO.fetch_website_thread()
+    stop = timeit.default_timer()
+    print(f'Test Fetch Website Thread Total: {stop - start}')

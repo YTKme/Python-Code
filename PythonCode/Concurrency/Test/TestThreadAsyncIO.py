9d346f3f6368f4101565d0a6f10e9db597e9f917
Yan Kuang <YTKme@Outlook.com>
2024-05-06 09:31:57 -0700

    Add asyncio function for ThreadAsyncIO module

--- PythonCode/Concurrency/Test/TestThreadAsyncIO.py
+++ PythonCode/Concurrency/Test/TestThreadAsyncIO.py
@@ -12,6 +12,17 @@ import pytest
 from PythonCode.Concurrency import ThreadAsyncIO
 
 
+@pytest.mark.asyncio
+async def test_fetch_website_async():
+    """Test Fetch Website Async"""
+
+    # Execute
+    start = timeit.default_timer()
+    await ThreadAsyncIO.fetch_website_async()
+    stop = timeit.default_timer()
+    print(f'Test Fetch Website Async Total: {stop - start}')
+
+
 def test_fetch_website_thread():
     """Test Fetch Website Thread"""
 

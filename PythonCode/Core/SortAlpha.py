dc44fec427bf27dd2ccf44a96f7dcb7088a310b4
Yan Kuang <YTKme@Outlook.com>
2025-09-28 21:54:31 -0700

    Update SortAlpha module and test

--- PythonCode/Core/SortAlpha.py
+++ PythonCode/Core/SortAlpha.py
@@ -5,6 +5,7 @@ Sort Alpha
 The SortAlpha module provide example(s) to sort data structure(s).
 """
 
+
 def sort_dictionary_by_key(data: dict, reverse: bool = False) -> dict:
     """Sort Dictionary By Key
 
@@ -21,6 +22,29 @@ def sort_dictionary_by_key(data: dict, reverse: bool = False) -> dict:
     """
 
     return dict(sorted(
-        iterable=data.items(),
+        data.items(),
         key=lambda item: item[0],
+        reverse=reverse,
+    ))
+
+
+def sort_dictionary_by_value(data: dict, reverse: bool = False) -> dict:
+    """Sort Dictionary By Value
+
+    Sort a dictionary by its value(s) in the specified order.
+
+    :param data: The dictionary to be sorted.
+    :type data: dict
+    :param reverse: Whether to sort in descending order, default to
+        False (ascending order).
+    :type reverse: bool
+
+    :return: The sorted dictionary.
+    :rtype: dict
+    """
+
+    return dict(sorted(
+        data.items(),
+        key=lambda item: item[1],
+        reverse=reverse,
     ))
--- PythonCode/Core/Test/TestSortAlpha.py
+++ PythonCode/Core/Test/TestSortAlpha.py
@@ -3,5 +3,61 @@ Test Sort Alpha Class
 ~~~~~~~~~~~~~~~~~~~~~
 """
 
+
+import pytest
+
+from PythonCode.Core.SortAlpha import sort_dictionary_by_key
+from PythonCode.Core.SortAlpha import sort_dictionary_by_value
+
+
 class TestSortAlpha:
     """Test Sort Alpha Class"""
+
+    @pytest.mark.parametrize("data, reverse, expected", [
+        (
+            {"Toyota": 3, "Honda": 1, "Ford": 4, "BMW": 2},
+            False,
+            {"BMW": 2, "Ford": 4, "Honda": 1, "Toyota": 3},
+        ),
+        (
+            {"Banana": 5, "Apple": 2, "Orange": 8, "Grapes": 3},
+            False,
+            {"Apple": 2, "Banana": 5, "Grapes": 3, "Orange": 8},
+        ),
+        (
+            {"Zebra": 7, "Elephant": 4, "Lion": 6, "Tiger": 5},
+            True,
+            {"Zebra": 7, "Lion": 6, "Tiger": 5, "Elephant": 4},
+        ),
+    ])
+    def test_sort_dictionary_by_key(self, data, reverse, expected):
+        """Test Sort Dictionary By Key"""
+
+        data = sort_dictionary_by_key(data, reverse=reverse)
+
+        assert data == expected
+
+
+    @pytest.mark.parametrize("data, reverse, expected", [
+        (
+            {"Toyota": 3, "Honda": 1, "Ford": 4, "BMW": 2},
+            False,
+            {"Honda": 1, "BMW": 2, "Toyota": 3, "Ford": 4},
+        ),
+        (
+            {"Banana": 5, "Apple": 2, "Orange": 8, "Grapes": 3},
+            False,
+            {"Apple": 2, "Grapes": 3, "Banana": 5, "Orange": 8},
+        ),
+        (
+            {"Zebra": 7, "Elephant": 4, "Lion": 6, "Tiger": 5},
+            True,
+            {"Zebra": 7, "Lion": 6, "Tiger": 5, "Elephant": 4},
+        ),
+    ])
+    def test_sort_dictionary_by_value(self, data, reverse, expected):
+        """Test Sort Dictionary By Value"""
+
+        data = sort_dictionary_by_value(data, reverse=reverse)
+
+        assert data == expected
